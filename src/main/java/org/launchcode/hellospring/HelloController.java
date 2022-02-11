30b72148bdd49299916f18b03b5cfc5bbc7d4f13
Michelle Ferris <jmichellelim@yahoo.com>
2022-02-11 10:04:52 -0500

    initial commit

---
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -0,0 +1,47 @@
+package org.launchcode.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+//automatically maps hello before every single method
+//@RequestMapping(value = "hello")
+public class HelloController {
+    //response to requests to the root path: /
+    @GetMapping("")
+    @ResponseBody
+        public String hello() {
+        return "Hello, Spring!";
+        }
+
+        //response to requests at /bonjour
+        @GetMapping("bonjour")
+    @ResponseBody
+    public String bonjour() {
+        return "Bonjour, Spring!";
+        }
+        // responds to GET requests like /hello?name=Michelle
+        @GetMapping("hello")
+    @ResponseBody
+    public String helloQuery(@RequestParam String name) {
+        return "Hello, "+name + "!";
+        }
+        //responds to GET request like /hi/Michelle
+        @GetMapping("hi/{name}")
+    @ResponseBody
+    public String hiQuery(@PathVariable String name) {
+        return "Hello, "+name+"!";
+        }
+
+        @GetMapping("hello-forms")
+    @ResponseBody
+    public String helloForms() {
+        return "<form method = 'POST'><input name = 'name' type = 'text'><button type = 'submit'>Greet Me</form>";
+        }
+
+        @PostMapping("hello-forms")
+    @ResponseBody
+    public String helloForms(@RequestParam String name) {
+        return "Hello, "+name+"!";
+        }
+}
